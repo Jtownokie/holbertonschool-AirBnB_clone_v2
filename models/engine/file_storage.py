3311eb250286f9f4f1c63effc55a0d990ac13b7e
Jtownokie <6673@holbertonstudents.com>
2023-11-11 14:06:49 -0600

    Corrected model files, completed int casting Flask route task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except Exception:
             pass
 
     def delete(self, obj=None):
@@ -64,4 +64,3 @@ class FileStorage:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
-
