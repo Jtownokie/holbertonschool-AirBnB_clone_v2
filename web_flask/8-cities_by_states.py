24b2107b551eed9b38528d20230cf3b17a912099
Jtownokie <6673@holbertonstudents.com>
2023-11-12 19:33:16 -0600

    Corrected route name

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """ Simple Hbnb Function """
-    return render_template('7-states_list.html',
+    return render_template('8-cities_by_states.html',
                            state_table=storage.all(State))
 
 
